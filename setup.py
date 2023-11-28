9fabf9070abf2de833239ac707e2b924f384d034
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2023-11-28 20:14:53 -0300

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pitsu',
+    version='1.0',
+    packages=['pitsu', 'pitsu/element'],
+    packages_data={'pitsu', 'pitsu/element/_base.pyi'},
+    data_files=[('pitsu', ['pitsu/element/_base.pyi'])],
+    author='Filipi565',
+    description='a python module to create html pages with python code',
+)
