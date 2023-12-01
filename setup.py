60a3ad17558253f204b3dd4ef2a1d9ec23622c3a
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2023-11-30 21:53:01 -0300

    update

--- setup.py
+++ setup.py
@@ -12,14 +12,13 @@ with open(os.path.join(HERE, 'LICENSE')) as f:
 setup(
     name='pitsu',
     version='1.0.6',
-    packages=['pitsu', 'pitsu/element'],
     author='Filipi565',
     description='a python module to create html pages with python code',
     long_description_content_type='text/markdown',
     long_description=DESCRIPTION,
     url='https://github.com/Filipi565/pitsu',
     license=LICENSE,
-    requires=[
-        
-    ]
+    packages=[
+        'pitsu', 'pitsu/element'
+    ],
 )
