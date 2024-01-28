e65259dfe30a8a9b89e6dabec3bf516c17b7051e
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2024-01-28 10:38:07 -0300

    Remove Old Directorys

--- src/pitsu/__init__.py
+++
@@ -1,8 +0,0 @@
-class ElementError(Exception):
-    """Error by Element class"""
-class ClassError(Exception):
-    """Error by Class_List class"""
-
-from .element import *
-
-__version__ = "1.3.0"
