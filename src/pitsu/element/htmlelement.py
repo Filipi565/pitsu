7209baf0b3017d12b9ec71be6fd719793288c404
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2024-02-24 11:54:13 -0300

    Bug Fix

--- src/pitsu/element/htmlelement.py
+++
@@ -1,9 +0,0 @@
-from ..typing import Child
-from .element import Element
-
-class HtmlElement(Element):
-    def __init__(self, *children: Child, **attributes: str):
-        super().__init__("html", *children, **attributes)
-    
-    def pack(self) -> str:
-        return "<!DOCTYPE html>\n" + super().pack()
